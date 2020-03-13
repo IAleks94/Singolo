c8316c4de497eab4bb733079a8e9ec2d2d9087fc
IAleks94 <a12344@mail.ru>
2020-03-13 20:13:06 +0300

    fead: add implement menu

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 'use strict'
+document.addEventListener('click', (evt) => clickHendler(evt));
+
+let arrMenuLink = document.querySelectorAll('.menu-link');
+
+function clickHendler(evt) {
+ let target = evt.target;
+ if (target.classList.contains('menu-link')) {
+    arrMenuLink.forEach(item => item.classList.remove('menu-activ'))
+    target.classList.add('menu-activ');
+ }
+ 
+ //console.log(target);
+}
